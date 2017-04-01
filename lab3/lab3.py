57693cecdbdd1438469b9cb9e22eba7ba54d2922
Pandas <daria-shcherbatyuk@yandex.ru>
2017-04-01 14:31:20 +0300

    add module with gauss and seidel methods

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,13 +1,13 @@
 from PyQt5 import QtWidgets, uic
-from  math import sin, pi, fabs, pow
+from math import sin, pi, fabs, pow
 from random import uniform
 import numpy as np
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QTableWidgetItem
 import matplotlib.pyplot as plt
+import lin_sys
 
-#TODO если получен сигнал изменения ячейки счиать
 
 
 def f(x):
@@ -25,7 +25,8 @@ class Window(QtWidgets.QMainWindow):
         self.xss = []
         self.yss = []
         self.pss = []
-        self.css = None
+        self.css_np = None
+        self.css = []
         self.err_up = None
         self.err_down = None
 
@@ -37,10 +38,6 @@ def fill_table(win, func):
     h = win.x_h.value()
     win.err_up = win.eps_b.value()
     win.err_down = win.eps_e.value()
-    win.xss = []
-    win.yss = []
-    win.pss = []
-
     c = fabs(a - b) / h
     cr = win.table.rowCount()
     if c > cr:
@@ -53,14 +50,14 @@ def fill_table(win, func):
         r = uniform(win.err_up, win.err_down)
         item_y = QTableWidgetItem("{0:.3f} ".format(func(a) + r))
         item_p = QTableWidgetItem("{0:.2f}".format(1))
-        win.xss.append(a)
+        """win.xss.append(a)
         win.yss.append(func(a) + r)
-        win.pss.append(1)
+        win.pss.append(1)"""
         win.table.setItem(i, 0, item_x)
         win.table.setItem(i, 1, item_y)
         win.table.setItem(i, 2, item_p)
-
         a += h
+
 
 def read_table(win):
     win.xss = []
@@ -69,12 +66,17 @@ def read_table(win):
     r = win.table.rowCount()
     for i in range(r):
         item_x = win.table.item(i, 0)
+        win.xss.append(float(item_x.text()))
+    for i in range(r):
         item_y = win.table.item(i, 1)
+        win.yss.append(float(item_y.text()))
+    for i in range(r):
         item_p = win.table.item(i, 2)
-        print(i, item_x.text())
-        print("\n")
+        win.pss.append(float(item_p.text()))
+
 
 def solve(win):
+    read_table(win)
     if len(win.xss) == 0 or len(win.yss) == 0:
         QtWidgets.QErrorMessage(win).showMessage("Таблица не заполнена!", 'warning')
     else:
@@ -89,16 +91,22 @@ def solve(win):
             for i in range(N):
                 B[j] += win.yss[i] * pow(win.xss[i], j) * win.pss[i]
 
-        D = np.linalg.inv(A)
-        D = np.matrix(D)
-        C = D * B
+        if win.lib.isChecked():
+            D = np.linalg.inv(A)
+            D = np.matrix(D)
+            C = D * B
+            C = C.transpose()
+            win.css_np = np.array(C.ravel())
+            win.css_np = nparray_to_list(win.css_np)[0]
+            #draw(win)
 
-        C = C.transpose()
-        win.css = C.ravel()
+        nparray_to_list(A)
+        nparray_to_list(B)
+        win.css = lin_sys.seidel(A, B, 0.0001)
         draw(win)
 
 
-def approx_func(win):
+def approx_func(win, coefs):
     n = win.d.value() + 1
     a = win.x_b.value()
     b = win.x_e.value()
@@ -107,7 +115,7 @@ def approx_func(win):
     for x in xs:
         y = 0
         for j in range(n):
-            y += win.css[0, j] * pow(x, j)
+            y += coefs[j] * pow(x, j)
         ys.append(y)
 
     return xs, ys
@@ -123,11 +131,16 @@ def draw(win):
         b = win.x_e.value()
         real_x = np.arange(a, b, 0.01)
         real_y = [f(x) for x in real_x]
-        app_x, app_y = approx_func(win)
+        app_x, app_y = approx_func(win, win.css)
+
         plt.figure("Среднеквадратичное приближение")
-        plt.plot(x, y, color='r', linewidth=1.0, label="Фактический результат")
-        plt.plot(real_x, real_y, color='g', linewidth=1.0, label="Реальный результат")
+        plt.scatter(x, y, color='r', s=10, alpha=.5, label="Фактический результат")
+
+        if win.lib.isChecked():
+            np_x, np_y = approx_func(win, win.css_np)
+            plt.plot(np_x, np_y, color='m', linewidth=1.0, label="NumPy")
         plt.plot(app_x, app_y, color='b', linewidth=1.0, label="Аппроксимация")
+        plt.plot(real_x, real_y, color='black', linewidth=0.5, label="Реальный результат")
         plt.grid(True)
         plt.xlabel(u'X')
         plt.ylabel(u'Y')
@@ -138,6 +151,12 @@ def draw(win):
 def add_row(win):
     win.table.insertRow(win.table.rowCount())
 
+
+def nparray_to_list(a):
+    a = list(a)
+    for i in range(len(a)):
+        a[i] = list(a[i])
+    return a
 
 if __name__ == "__main__":
     import sys
---
+++ lab3/lin_sys.py
@@ -0,0 +1,58 @@
+""" The 'gauss' function takes two matrices, 'a' and 'b', with 'a' square, and it return the determinant of 'a' 
+and a matrix 'x' such that a*x = b.
+If 'b' is the identity, then 'x' is the inverse of 'a'"""
+
+import copy
+from math import sqrt
+
+
+def gauss(a, b):
+    a = copy.deepcopy(a)
+    b = copy.deepcopy(b)
+    n = len(a)
+    p = len(b[0])
+    det = 1
+    for i in range(n - 1):
+        k = i
+        for j in range(i + 1, n):
+            if abs(a[j][i]) > abs(a[k][i]):
+                k = j
+        if k != i:
+            a[i], a[k] = a[k], a[i]
+            b[i], b[k] = b[k], b[i]
+            det = -det
+
+        for j in range(i + 1, n):
+            t = a[j][i] / a[i][i]
+            for k in range(i + 1, n):
+                a[j][k] -= t * a[i][k]
+            for k in range(p):
+                b[j][k] -= t * b[i][k]
+
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
+            t = a[i][j]
+            for k in range(p):
+                b[i][k] -= t * b[j][k]
+        t = 1 / a[i][i]
+        det *= a[i][i]
+        for j in range(p):
+            b[i][j] *= t
+    return det, b
+
+
+def seidel(A, b, eps):
+    n = len(A)
+    x = [0 for i in range(n)]
+
+    converge = False
+    while not converge:
+        x_new = x.copy()
+        for i in range(n):
+            s1 = sum(A[i][j] * x_new[j] for j in range(i))
+            s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
+            x_new[i] = (b[i] - s1 - s2) / A[i][i]
+
+        converge = sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(n))) <= eps
+        x = x_new
+    return x
