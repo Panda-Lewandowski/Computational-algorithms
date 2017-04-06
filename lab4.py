63c23eeefd88e8098bc7ebd3069e090fa8214fb0
Pandas <daria-shcherbatyuk@yandex.ru>
2017-04-06 13:58:00 +0300

    add polynomial formulas

--- lab4.py
+++ lab4.py
@@ -2,6 +2,7 @@
 from  math import pi, fabs, e, pow, log
 import numpy as np
 from prettytable import PrettyTable
+from itertools import combinations
 
 
 def f(x):
@@ -23,12 +24,14 @@ def left_side_diff(ys, h):
         l.append((ys[i] - ys[i - 1]) / h)
     return l
 
+
 def right_side_diff(ys, h):
-    l =[]
+    l = []
     for i in range(0, len(ys) - 1):
         l.append((ys[i + 1] - ys[i]) / h)
     l.append(None)
     return l
+
 
 def centre_diff(ys, h):
     l = [None]
@@ -37,6 +40,7 @@ def centre_diff(ys, h):
     l.append(None)
     return l
 
+
 def extreme_points(ys, h):
     l = []
     l.append((-3 * ys[0] + 4 * ys[1] - ys[2]) / (2 * h))
@@ -44,6 +48,7 @@ def extreme_points(ys, h):
         l.append(None)
     l.append((ys[len(ys) - 3] - 4 * ys[len(ys) - 2] + 3 * ys[len(ys) - 1]) / (2 * h))
     return l
+
 
 def runge(ys, h, r):
     l = [None, None]
@@ -75,22 +80,26 @@ def divided_difference(xs, ys):
     else:
         return (divided_difference(xs[:-1], ys[:-1]) - divided_difference(xs[1:], ys[1:])) / (xs[0] - xs[l - 1])
 
+
 def polinomial(xs, ys, x):
-    y = divided_difference(xs[:2], ys[:2])
-    for i in range(1, len(xs)):
-        sample_x = xs[:i + 2]
-        sample_y = ys[:i + 2]
-        k = 1
-        for j in range(i + 1):
-            k *= (x - sample_x[j])
-        dd = divided_difference(xs[:i+1], ys[:i+1])
-        y += (k * dd)
-    return y
+    z = [x - xi for xi in xs]
+    y_on_x = divided_difference(xs[:2], ys[:2])
+    for i in range(1, len(z)):
+        it = combinations(z[:i + 1], i)
+        sum = 0
+        for k in it:
+            if k is not None:
+                p = 1
+                for i in range(len(k)):
+                    p *= k[i]
+                sum += p
+        y_on_x += (sum * divided_difference(xs[:i + 2], ys[:i + 2]))
+    return y_on_x
+
 
 a = float(input('Введите нижнюю границу значений Х: '))
 b = float(input('Введите верхнюю границу значений Х: '))
 h = float(input('Введите шаг: '))
-
 
 xss, yss = get_table(a, b, h, f)
 left = left_side_diff(yss, h)
@@ -109,8 +118,10 @@ table.add_column("Центральная", centre)
 table.add_column("Повышенная точность", exrt)
 table.add_column("Рунге", r)
 table.add_column("Выравнивающие", lev)
-print("+-----+--------------------+-----------------------------------------------------------------------------------------------------------------------------+\n"
-      "|                          |                                                y'(x)                                                                        |")
+print(
+    "+-----+--------------------+-----------------------------------------------------------------------------------------------------------------------------+\n"
+    "|                          |                                                y'(x)                                                                        |")
 print(table)
-
-print(polinomial(xss, yss, 1.5), "----", e ** 1.5)
+x = float(input('Введите  Х: '))
+print("\nПолиномиальные формулы:", polinomial(xss, yss, x), '\n')
+print("Реальный результат: ", f(x))
